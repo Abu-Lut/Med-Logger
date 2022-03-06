c7ceddb7fdca5a2fe01abc095072b8a7d9e18eaf
Abu-Lut <muimkh123@gmail.com>
2022-03-06 20:57:11 +0530

    test

--- main.js
+++ main.js
@@ -121,11 +121,11 @@ if (time>= 0 && time<=8) {
 let alertTime = ''
 
 if (session=='morning'){
-    alertTime = '0 0/20 7 * * *' 
+    alertTime = '0 0/15 7 * * *' 
 }else if (session=='afternoon'){
-    alertTime = '0 0/20 14 * * *' 
+    alertTime = '0 0/15 14 * * *' 
 }else if (session=='night'){
-    alertTime = '0 0/15 20,22 * * *' 
+    alertTime = '0 * 20,22 * * *' 
 }
 
 // messages to be sent 
