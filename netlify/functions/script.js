50b8697a51cd36e863e3b5b19a9115de51107550
Abu-Lut <muimkh123@gmail.com>
2022-03-09 02:41:30 +0530

    net

---
+++ netlify/functions/script.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'Medicines'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+.then(client => {
+    console.log(`Connected to ${dbName} Database`)
+    db = client.db(dbName)
+})
+.catch( error => console.error(error))
+
+
+
+const {schedule} = require('@netlify/functions')
+
+export const handler = schedule("* * * * *", async function checkMeds(){
+    console.log("first try bismillaah")
+    const result = await db.collection("Medicines").find({
+        medTiming: session,
+        medStatus: "false"
+    })
+
+    if (result){
+        await result.forEach(item => {
+            bot.sendMessage(chatId, `Munavar Sultana hasn't taken her ${item.medName} medicine this ${session} on ${today}, please remind her to take her medicines`)
+                }
+            )
+        }
+    }
+)
